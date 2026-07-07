31ee988f52b453f158119504ebbbba79fc472167
samarthg-dot <samarthg@motorola.com>
2026-07-07 17:10:17 +0530

    Update auto-refresh.tsx

--- components/auto-refresh.tsx
+++ components/auto-refresh.tsx
@@ -9,7 +9,7 @@ export function AutoRefresh() {
   useEffect(() => {
     const interval = setInterval(() => {
       router.refresh()
-    }, 30 * 1000)
+    }, 5 * 60 * 1000)
 
     return () => clearInterval(interval)
   }, [router])
